f8078da33422da35521972a206a504aba22c10f2
Ethan Arns <ethanarns95@gmail.com>
2020-07-03 17:19:09 -0700

    Added unparsed/string splitter

--- src/torquecompile/TorqueCompile.java
+++ src/torquecompile/TorqueCompile.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 
 public class TorqueCompile {
     public static void main(String[] args) throws Exception {
+        System.out.println("Starting...");
         TorqueCompile tcomp = new TorqueCompile();
-        String testCode = "string str = \"asdf\";";
+        String testCode = "string str = \"asdf\"; string str2 = \"fdsa\";";
         tcomp.parseStrings(testCode);
     }
 
@@ -13,28 +14,21 @@ public class TorqueCompile {
 
     }
 
-    public int endOfString(int curIndex, String str) {
-        int finalIndex = curIndex;
-        System.out.println("Starting index: " + curIndex);
-        for (int i = curIndex + 1; i < str.length(); i++) {
-            if (str.charAt(i) == '"') {
-                return finalIndex;
-            }
-        }
-        return -1;
-    }
-
     public void parseStrings(String li) {
         li = li.trim();
         ArrayList<Token> tokens = new ArrayList<Token>();
+        boolean inString = false;
+        String built = "";
         for (int i = 0; i < li.length(); i++) {
             char c = li.charAt(i);
+            built += c;
             if (c == '"') {
-                int endIndex = endOfString(i, li);
-                String sub = li.substring(i, endIndex-1);
-                System.out.println(sub);
+                tokens.add(new Token(inString ? Lexeme.STRING : Lexeme.UNPARSED, built));
+                inString = !inString;
+                built = "";
             }
         }
+        tokens.add(new Token(inString ? Lexeme.STRING : Lexeme.UNPARSED, built));
         for (Token token : tokens) {
             System.out.println(token);
         }
